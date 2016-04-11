512e8926ddbb45d77baab1e7bd6252682d1796a9
dgilbs <danielgilberg@gmail.com>
2016-04-11 02:33:17 -0400

    rewrites user model logic to incorporate the possibility of nil values

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,13 +39,15 @@ class User < ActiveRecord::Base
   end
 
   def past_trips
-    self.planned_trips.select do |trip|
+    trips = self.planned_trips.select do |trip|
       trip.start_date < Date.today
     end
   end
 
   def planned_trips
-    self.trips.select {|trip| trip.start_date != nil}
+    if !self.trips.nil?
+      self.trips.select {|trip| trip.start_date != nil}
+    end
   end
 
   def next_trip
@@ -59,46 +61,69 @@ class User < ActiveRecord::Base
   end
 
   def destinations_visited
-   arr = self.past_trips.map do |trip|
-    trip.destinations if !trip.destinations.nil?
-    end
-   arr[0]
+   if !self.past_trips.nil? 
+     arr = self.past_trips.map do |trip|
+      trip.destinations if !trip.destinations.nil?
+      end
+     arr[0]
+   end
   end
 
   def cities_visited
-    self.destinations_visited.map do |dest|
-      dest.city
-    end.compact.uniq
+    if !self.destinations_visited.nil?
+      self.destinations_visited.map do |dest|
+        dest.city 
+      end.compact.uniq
+    end
   end
 
   def countries_visited
-    self.destinations_visited.map do |dest|
-      dest.country
-    end.compact.uniq
+    if !self.destinations_visited.nil?
+      self.destinations_visited.map do |dest|
+        dest.country
+      end.compact.uniq
+    end
   end  
 
   def cities_by_popularity
-    grouped_destinations = self.destinations_visited.group_by {|destination| destination.city}
-    grouped_destinations.sort_by{|destination, visits| visits.length}
+    if !self.cities_visited.nil?
+      grouped_destinations = self.destinations_visited.group_by {|destination| destination.city}
+      grouped_destinations.sort_by{|destination, visits| visits.length}
+    end
   end
 
-  def favorite_city
+  def countries_by_popularity
+    if !self.countries_visited.nil?
+      grouped_destinations = self.destinations_visited.group_by {|destination| destination.country}
+      grouped_destinations.sort_by{|destination, visits| visits.length}
+    end
+  end  
+
+  def find_favorite_city
     array = self.cities_by_popularity.map do |cities|
       cities[0]
     end
     array.last
   end
 
-  def countries_by_popularity
-    grouped_destinations = self.destinations_visited.group_by {|destination| destination.country}
-    grouped_destinations.sort_by{|destination, visits| visits.length}
-  end
-
-  def favorite_country
+  def find_favorite_country
     array = self.countries_by_popularity.map do |countries|
       countries[0]
     end
     array.last
+  end
+
+  def favorite_city
+    if !self.cities_by_popularity.nil?
+      self.find_favorite_city
+    end
+  end
+
+
+  def favorite_country
+    if !self.countries_visited.nil?
+      self.find_favorite_country
+    end
   end
 
   def items_by_popularity
