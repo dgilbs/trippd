4e06c5de24597468f8c1e01ce3af18ff8846cdaf
dgilbs <danielgilberg@gmail.com>
2016-04-14 18:59:19 -0400

    works on jquery for packing lists

--- app/assets/javascripts/tripShow.js
+++ app/assets/javascripts/tripShow.js
@@ -14,25 +14,29 @@ $(document).on("ready", function(){
       var id = response.id
 
       $("#new-packing-list").append('<div class="packing-list"> <h4>'+ name + '</h4><form id="new-item-form" action="/items" method="POST"><input type="text" id="new-item-name" placeholder="Add an item!"><input type="hidden" id="packing_list_id" name="packing_list_id" value="' + id +'"><input type="submit" id="submit-item-btn"></form><ul id="packing-list-' + id +'"></ul></div>');
-
+      $("#new_packing_list").hide()
     })
     
 
   })
 
-  $(document).on('click', '#submit-item-btn', function(event){
+  $(document).on('click', '.submit-item-btn', function(event){
     event.preventDefault();
     var name = $("#new-item-name").val();
-    var packing_list_id = $("#packing_list_id").val()    
+    var packing_list_id = $("#packing_list_id").val() 
+    var string = "#packing-list-" + packing_list_id   
     $.ajax({
       url: '/items',
       method:"POST",
       data: {name: name, packing_list_id: packing_list_id}
     }).success(function(response, settings){
-      $("#packing-list-" + response.packing_list_id).append("<li>" + response.name + "</li>")
+      $('#packing-list-' + packing_list_id).append("<li>" + response.name + "</li>")
+      $("#new-item-name").val("")
       
     })
   })
+
+
 
 })
 
