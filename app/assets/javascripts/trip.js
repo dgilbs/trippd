98eef0ca0ae7a572675ad4c7467b32a006d41488
mallorycoppenrath <malcoppenrath@gmail.com>
2016-04-15 16:17:06 -0400

    Revert "glyphicon"
    This reverts commit aeb9e7f0f4d98d9b2521594a61f1fd0bd1a518ad, reversing
    changes made to 61b1cf7e4513bd7ae8875e19b00105bf363b665e.

--- app/assets/javascripts/trip.js
+++ app/assets/javascripts/trip.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
    $(".email-trip-btn").click(function(event){
       event.preventDefault();
@@ -12,18 +11,3 @@ $(document).ready(function() {
     })
     })
 });
-=======
-// $(document).on("page:load ready", function() {
-//    $(".email-trip-btn").click(function(event){
-//       event.preventDefault();
-//       var id = $(".email-trip-btn").val()
-//       $.ajax({
-//         url: '/email_trip',
-//         method: "POST",
-//         data: {id: id}
-//         }).success(function(response, setting){
-//       alert("Your trip was emailed! :) :)")
-//     })
-//     })
-// });
->>>>>>> 5c8df1a9102acd8da390106bcd59c4b22dd22a19
